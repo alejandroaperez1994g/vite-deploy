1c07eb5f9f49dc2c27a425680b60a1b0cd27135a
Alejandro <alejandroaperez1994g@gmail.com>
2023-10-10 20:59:41 +0200

    feat: watch redirect uri

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import {Auth0Provider} from "@auth0/auth0-react";
 
 const {VITE_AUTH0_DOMAIN: domain, VITE_AUTH0_CLIENT_ID: clientId,VITE_AUTH0_AUDIENCE: audience} = import.meta.env
 const redirectUri = window.location.origin + "/home"
+console.log({redirectUri})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
